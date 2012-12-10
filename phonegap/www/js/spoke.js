795da5be7c825dd78417ba72afe8e79650ec4877
Steven Day <steve@mysociety.org>
2012-12-10 17:44:17 +0000

    Begin switch to media api instead of capture api, create a file on record button click

--- phonegap/www/js/spoke.js
+++ phonegap/www/js/spoke.js
@@ -1,7 +1,7 @@
 /**
  * Spoke/SayIt javascript
  */
- ;(function($, SPOKE) {
+ ;( function ($, SPOKE) {
 
     // Use jQuery Deffered to track when both jquery mobile, phonegap, 
 	// and the first page are ready
@@ -9,40 +9,124 @@
         jqmReady = $.Deferred(),
         firstPageReady = $.Deferred();
 
-    // Options for phonegap audio capture
-    var speechCaptureOptions = {
-        limit: 1, // > 1 means more than one recording at a time
-        duration: 120, // Max length in seconds
-    };
-
     /**
      * Functions 
      */
 
-    function speechCaptureSuccess(mediaFiles) {
+    function speechCaptureSuccess (mediaFiles) {
         console.log("Captured Audio:")
-        console.log(mediaFiles);
+        console.log(mediaFiles[0]);
     }  
 
-    function speechCaptureError(error) {
+    function speechCaptureError (error) {
         console.log("Error capturing audio:");
         console.log(error);
     }
     
-    function recordPage() {
+    function recordPage () {
     	console.log("Binding record page click handlers")
     	$("#record-button").on("tap", function(e) {
             e.preventDefault();
 
-            console.log("Capturing Audio");
+            console.log("Record Audio button clicked");
 
-            // Start recording a new audio file
-            var capture = navigator.device.capture;
-            capture.captureAudio(
-                speechCaptureSuccess,
-                speechCaptureError,
-                speechCaptureOptions);
+            recordAudio();
         });
+    }
+    
+    function recordAudio () {
+    	
+    	console.log("Recording Audio");
+        
+        // Get a file to record to - iOS needs this, others don't care so much
+
+        // fileCreated is a promise because the Phonegap filesystem apis are
+        // asynchronous
+    	creatingFile = createFile();
+
+        creatingFile.done( function (file) {
+
+            // We have a FileEntry to play with in file
+            console.log("File created: " + file);
+
+            // TODO - Start the recording
+
+            // TODO - show a recording interface whilst this is happening
+            // so people can start/stop the recording
+        });
+
+        creatingFile.fail(function(error) {
+
+            // Something went wrong creating the file
+            console.log("Something went wrong creating a file: " + error);
+
+        });
+
+    }
+    
+    // Create a new file to put our recording's into
+    function createFile () {
+    	
+    	console.log("Creating a file");
+    	
+        // Chain a bunch of promises together to return one thing
+        // which encapsulates all the async calls
+        return getFileSystem()
+            .pipe( function (filesystem) {
+            	
+            	console.log("Filesystem returned: " + filesystem);
+            	
+                return getDirectory(filesystem.root, SPOKE.audioDirectory, {create: true});
+            })
+            .pipe( function (directory) {
+            	
+            	console.log("Directory returned: " + directory);
+
+            	// Create a new file
+                filename = SPOKE.recordingsCounter + SPOKE.audioFilenameExtension;
+                return getFile(directory, filename, {create: true, exclusive: true});
+            });
+    }
+
+    // Wrap the async Phonegap way of getting a filesystem in a promise
+    function getFileSystem () {
+    	
+    	console.log("Getting the file system");
+    	
+        var filesystem = $.Deferred();
+
+        window.requestFileSystem(
+            LocalFileSystem.PERSISTENT, 
+            0,
+            filesystem.resolve, 
+            filesystem.reject
+        );
+
+        return filesystem.promise();
+    }
+
+    // Wrap the async Phonegap way of getting a directory in a promise
+    function getDirectory (rootDirectory, path, options) {
+    	
+    	console.log("Getting a directory");
+    	
+        directory = $.Deferred();
+
+        rootDirectory.getDirectory(path, options, directory.resolve, directory.reject);
+
+        return directory.promise();
+    }
+
+    // Wrap the async Phonegap way of getting a file in a promise
+    function getFile (directory, path, options) {
+    	
+    	console.log("Getting a file");
+    	
+        file = $.Deferred();
+
+        directory.getFile(path, options, file.resolve, file.reject);
+
+        return file.promise();
     }
 
     /**
@@ -65,6 +149,15 @@
     $.when(jqmReady, pgReady, firstPageReady).then(function() {
         // Everything is ready now
     	console.log("Initialising the SayIt app");
+
+        // Globals
+        $.extend(SPOKE, {
+            // Details for audio capture
+            audioDirectory: "spoke",
+            defaultAudioFilename: "recording",
+            audioFilenameExtension: (device.platform === "iPhone") ? ".wav" : ".amr",
+            recordingsCounter: 1
+        });
     	
     	// Fire up the record page manually, because a pageinit will have already
     	// happened for it, but probably before everything else was ready
