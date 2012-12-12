e77cf9e1a4d9fea2e96ba8a342dada9fd7772c29
Steven Day <steve@mysociety.org>
2012-12-12 12:52:31 +0000

    Add very basic audio recording, with start and stop buttons, and alist of recordings

--- phonegap/www/js/spoke.js
+++ phonegap/www/js/spoke.js
@@ -12,56 +12,123 @@
     /**
      * Functions 
      */
-
-    function speechCaptureSuccess (mediaFiles) {
-        console.log("Captured Audio:")
-        console.log(mediaFiles[0]);
-    }  
-
-    function speechCaptureError (error) {
-        console.log("Error capturing audio:");
-        console.log(error);
-    }
     
     function recordPage () {
-    	console.log("Binding record page click handlers")
-    	$("#record-button").on("tap", function(e) {
+
+    	console.log("Binding record page things")
+    	
+        $("#record-button").on("tap", function(e) {
             e.preventDefault();
 
             console.log("Record Audio button clicked");
 
-            recordAudio();
+            // We only want to record if we're not already, this
+            // could happen with dodgy click/tap/vlick handling or
+            // slow phones and angry users
+            if(typeof SPOKE.currentRecording === "undefined") {
+                // startRecordingAudio wraps the phonegap media creation api in
+                // a promise and returns it
+                var recordingAudio = startRecordingAudio();
+
+                // Success - save the filename of the file we created
+                recordingAudio.done(function() {
+
+                    console.log("Recording audio has finished");
+                    console.log("Filename: " + SPOKE.currentRecording.src + " Duration: " + SPOKE.currentRecording.getDuration());
+
+                    SPOKE.recordings.push(SPOKE.currentRecording.src);
+
+                    // TODO - what will we do with this eventually?
+                    // For now, just show the filename in a list
+                    $("ul#recorded-speeches")
+                        .append('<li>' + SPOKE.currentRecording.src + '</li>')
+                        .listview('refresh');
+
+                });
+
+                // Failure - nothing specific to do, except tell the user
+                recordingAudio.fail(function(error) {
+                    console.log("Something went wrong recording the audio: " + error.message);
+                });
+
+                // Whatever - delete the global which kept track of the recording
+                recordingAudio.always(function () {
+                    // Clear the current recording
+                    delete SPOKE.currentRecording;
+                });
+            }
+        });
+
+        $("#stop-button").on("tap", function(e) {
+            e.preventDefault();
+
+            console.log("Stop audio button clicked");
+
+            // We can only stop if there's something to stop
+            if(typeof SPOKE.currentRecording !== "undefined") {
+                stopRecordingAudio();
+            }
+                        
         });
     }
     
-    function recordAudio () {
+    function startRecordingAudio () {
     	
     	console.log("Recording Audio");
         
         // Get a file to record to - iOS needs this, others don't care so much
 
-        // fileCreated is a promise because the Phonegap filesystem apis are
+        // creatingFile and recordingAudio are promises because the Phonegap filesystem apis are
         // asynchronous
-    	creatingFile = createFile();
+    	var creatingFile = createFile(),
+            recordingAudio = $.Deferred();
 
         creatingFile.done( function (file) {
 
             // We have a FileEntry to play with in file
-            console.log("File created: " + file);
+            console.log("File created: " + file.fullPath);
 
-            // TODO - Start the recording
+            // Create a media object to actually do the recording
+            // iOS wants the file path to be full, ie: start with file://
+            // Android just wants it to be relative to the sdcard folder
+            var src = (device.platform === "iPhone") ? file.fullPath : SPOKE.audioDirectory + "/" + file.name;
+            var media = new Media(src, recordingAudio.resolve, recordingAudio.reject);
+            // Start the recording
+            media.startRecord();
+            // Save the media object so that we can stop it later
+            SPOKE.currentRecording = media;
+            // Show a recording interface so people can start/stop the recording
+            toggleRecordingControls();
 
-            // TODO - show a recording interface whilst this is happening
-            // so people can start/stop the recording
         });
 
         creatingFile.fail(function(error) {
 
-            // Something went wrong creating the file
-            console.log("Something went wrong creating a file: " + error);
+            // Something went wrong creating the file, and they only give us a code, great.
+            console.log("Something went wrong creating a file, error code: " + error.code);
 
         });
 
+        return recordingAudio;
+
+    }
+
+    function stopRecordingAudio() {
+
+        console.log("Stopping recording");
+
+        var media = SPOKE.currentRecording;
+
+        media.stopRecord();
+        
+        toggleRecordingControls();
+
+    }
+
+    // Toggle the 'recording/stop recording' controls
+    function toggleRecordingControls() {
+        $("#record-button").toggle();
+        $("#stop-button").toggle();
     }
     
     // Create a new file to put our recording's into
@@ -80,11 +147,13 @@
             })
             .pipe( function (directory) {
             	
-            	console.log("Directory returned: " + directory);
+            	console.log("Directory returned: " + directory.fullPath);
 
-            	// Create a new file
-                filename = SPOKE.recordingsCounter + SPOKE.audioFilenameExtension;
-                return getFile(directory, filename, {create: true, exclusive: true});
+            	// Create a new file, the path is relative to the directory we just got
+                // Use a timestamp to the nearest millisecond as a unique name
+                var timestamp = new Date().getTime();
+                path = "recording_" + timestamp + SPOKE.audioFilenameExtension;
+                return getFile(directory, path, {create: true, exclusive: true});
             });
     }
 
@@ -120,7 +189,7 @@
     // Wrap the async Phonegap way of getting a file in a promise
     function getFile (directory, path, options) {
     	
-    	console.log("Getting a file");
+    	console.log("Getting a file with path: " + path + " in directory: " + directory.fullPath);
     	
         file = $.Deferred();
 
@@ -156,7 +225,7 @@
             audioDirectory: "spoke",
             defaultAudioFilename: "recording",
             audioFilenameExtension: (device.platform === "iPhone") ? ".wav" : ".amr",
-            recordingsCounter: 1
+            recordings: Array()
         });
     	
     	// Fire up the record page manually, because a pageinit will have already
