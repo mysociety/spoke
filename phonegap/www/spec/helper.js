2c809bfe1cf464a97d1480cdc6f1d57c81fb1047
Steven Day <steve@mysociety.org>
2013-01-04 14:34:47 +0000

    Add helper functions for mocking phonegap file apis, to make testing possible

--- phonegap/www/spec/helper.js
+++ phonegap/www/spec/helper.js
@@ -3,15 +3,21 @@ afterEach(function() {
 });
 
 var helper = {
+    
+    // Trigger an event from a particular object
     trigger: function(obj, name) {
         var e = document.createEvent('Event');
         e.initEvent(name, true, true);
         obj.dispatchEvent(e);
     },
+
+    // Get the computed style for an element
     getComputedStyle: function(querySelector, property) {
         var element = document.querySelector(querySelector);
         return window.getComputedStyle(element).getPropertyValue(property);
     },
+
+    // Fake the starting up of a JQM + Phonegap app
     fakeAppStart: function() {
         // Create a fake jquery mobile page
         var pageDiv = window.document.createElement("div");
@@ -26,6 +32,81 @@ var helper = {
         helper.trigger(window.document, 'deviceready');
         helper.trigger(window.document, 'mobileinit');  
         helper.trigger(pageDiv, 'pageinit');
+    },
+
+    // Mock File object from Phonegap's File api
+    mockFile: function (name, fullPath) {
+        return {
+            isFile: true,
+            isDirectory: false,
+            name: name,
+            fullPath: fullPath,
+            remove: function(path, options, onSuccess, onError) {
+                onSuccess();
+            }
+        }
+    },
+
+    // Mock Directory object from Phonegap's File api
+    mockDirectory: function(name, fullPath, entries, reader) {
+        return {
+            isFile: false,
+            isDirectory: true,
+            name: name,
+            fullPath: fullPath,
+            getFile: function(path, options, onSuccess, onError) {
+                console.log(entries);
+                console.log(path);
+                if(entries.hasOwnProperty(path)) {
+                    onSuccess(entries[path]);
+                }
+                else {
+                    onError({code:1});
+                }
+            },
+            createReader: function() {
+                return reader;
+            },
+            getDirectory: function(path, options, onSuccess, onError) {
+                if(entries.hasOwnProperty(path)) {
+                    onSuccess(entries[path]);
+                }
+                else {
+                    onError({code:1});
+                }
+            },
+        }
+    },
+
+    // Function to create a mock DirectoryReader object
+    mockDirectoryReader: function(files) {
+        return {
+            readEntries: function (onSuccess, onFail) {
+                onSuccess(files);
+            }
+        }
+    },
+
+    // Function to create a mock FileSystem object from Phonegap's File api
+    mockFileSystem: function(name, root) {
+        return {
+            name: name,
+            root: root
+        }
+    },
+
+    // Function to create a mock requestFileSystem method from Phonegap's File api
+    mockRequestFileSystem: function(returnSuccess, fileSystem, error) {
+
+        return function(type, someNumber, onSuccess, onFail) {
+            
+            if(returnSuccess) {
+                onSuccess(fileSystem);
+            }
+            else {
+                onFail(error);
+            }
+        };
     }
 
 };
