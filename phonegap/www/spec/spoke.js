a8646f99e3412a252bbc8aa0a7b09c50c0359d97
Steven Day <steve@mysociety.org>
2013-01-04 10:33:30 +0000

    Add test for pageinit event binding

--- phonegap/www/spec/spoke.js
+++ phonegap/www/spec/spoke.js
@@ -1,12 +1,26 @@
-describe('app', function() {
+describe('Spoke', function() {
     
-    it("should fire the record page function when everything's ready", function() {
+    it("Should fire the record page function once when everything's ready", function () {
     	
 		spyOn(SPOKE.recordingPage, 'recordPage');
+    	
     	// We need to fake all the events which would happen
     	// because this won't be run in a mobile browser
     	helper.fakeAppStart();
+    	
     	expect(SPOKE.recordingPage.recordPage).toHaveBeenCalled();
+    	expect(SPOKE.recordingPage.recordPage.calls.length).toBe(1);
 
+    });
+
+    it("Should fire the record page function when the record page is init-ed", function () {
+    	
+    	spyOn(SPOKE.recordingPage, 'recordPage');
+    	
+    	helper.trigger(document.getElementById('record-page'), 'pageinit');
+
+    	expect(SPOKE.recordingPage.recordPage).toHaveBeenCalled();
+    	expect(SPOKE.recordingPage.recordPage.calls.length).toBe(1);
+    	
     });
 });
