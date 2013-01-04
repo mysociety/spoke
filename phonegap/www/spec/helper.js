d39d9e8508c16a8f872d61a85dd35e1669840298
Steven Day <steve@mysociety.org>
2013-01-04 09:19:50 +0000

    Add a helper method to fake all the events we need to simulate the app starting on a phone

--- phonegap/www/spec/helper.js
+++ phonegap/www/spec/helper.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 afterEach(function() {
     document.getElementById('stage').innerHTML = '';
 });
@@ -29,5 +11,21 @@ var helper = {
     getComputedStyle: function(querySelector, property) {
         var element = document.querySelector(querySelector);
         return window.getComputedStyle(element).getPropertyValue(property);
+    },
+    fakeAppStart: function() {
+        // Create a fake jquery mobile page
+        var pageDiv = window.document.createElement("div");
+        pageDiv.id = 'record-page';
+        $('body').append(pageDiv);
+
+        // create a fake device object
+        window.device = {
+            platform: "Android"
+        };
+
+        helper.trigger(window.document, 'deviceready');
+        helper.trigger(window.document, 'mobileinit');  
+        helper.trigger(pageDiv, 'pageinit');
     }
+
 };
