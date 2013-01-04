34746c70249a6ecf77f1f404b54100712f4a82ed
Steven Day <steve@mysociety.org>
2013-01-04 09:20:40 +0000

    Add a basic Jasmine test for the app startup

--- phonegap/www/spec/index.js
+++ phonegap/www/spec/index.js
@@ -1,3 +1,12 @@
 describe('app', function() {
     
+    it("should fire the record page function when everything's ready", function() {
+    	
+		spyOn(SPOKE.recordingPage, 'recordPage');
+    	// We need to fake all the events which would happen
+    	// because this won't be run in a mobile browser
+    	helper.fakeAppStart();
+    	expect(SPOKE.recordingPage.recordPage).toHaveBeenCalled();
+
+    });
 });
