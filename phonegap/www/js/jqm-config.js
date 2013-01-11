bb0667febef1032a6ba1b9bd19a2c858ff953908
Steven Day <steve@mysociety.org>
2013-01-11 10:46:58 +0000

    Add settings to turn off jqm routing so that backbone can work

--- phonegap/www/js/jqm-config.js
+++ phonegap/www/js/jqm-config.js
@@ -7,6 +7,18 @@
         $.mobile.pushStateEnabled = false;
         $.mobile.defaultPageTransition = 'none';
         $.mobile.buttonMarkup.hoverDelay = 0;
+
+        // Do some tricksy things so that jQuery mobile and Backbone
+        // play nicely (ie: jQM lets Backbone do the routing)
+        $.mobile.hashListeningEnabled = false;
+
+        // Remove page from DOM when it's being replaced, and unbind any events
+        // TODO - There is probably a nicer way to do this now with
+        // http://backbonejs.org/#Events-listenTo and http://backbonejs.org/#Events-stopListening
+        $(document).on('pagehide', 'div[data-role="page"]', function (event, ui) {
+            $(event.currentTarget).remove();
+            $(event.currentTarget).unbind();
+        });
     });
     
 })($);
