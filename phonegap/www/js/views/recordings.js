9c56a51e07b48b0e6b1f1cb75902af1ffef1a3dd
Steven Day <steve@mysociety.org>
2013-01-11 15:52:08 +0000

    Flesh out recordings view a little

--- phonegap/www/js/views/recordings.js
+++ phonegap/www/js/views/recordings.js
@@ -1,4 +1,36 @@
 /**
  * Recordings view - A list of recordings
  */
-;
+;(function (SPOKE, Backbone, _, $) {
+    _.extend(SPOKE, {
+        RecordingsView: Backbone.View.extend({
+
+            template: _.template($("#recordings-template").html()),
+
+            initialize: function (options) {
+                console.log('Recordings list initialising');
+                // TODO - give it a collection of recordings!
+            },
+
+            render: function () {
+                console.log('Recordings list rendering');
+
+                this.$el.html(this.template({recordings: {}}));
+
+                console.log(this.$el);
+
+                return this;
+            },
+
+            events: {
+                "vclick #upload-button": "uploadButton"
+            },
+
+            uploadButton: function (e) {
+                e.preventDefault();
+                
+                console.log('Uploading Audio');
+            }
+        }) 
+    });
+})(SPOKE, Backbone, _, $);
