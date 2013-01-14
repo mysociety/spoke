5f17c23aa4c28bd50ded6f8602d4f4d43029ac20
Steven Day <steve@mysociety.org>
2013-01-14 18:02:00 +0000

    Flesh out recording control event handlers to start and stop recording, and fire events for sub-views to use

--- phonegap/www/js/views/recording-controls.js
+++ phonegap/www/js/views/recording-controls.js
@@ -10,12 +10,18 @@
             initialize: function (options) {
                 console.log('Recording controls page initialising');
                 this.listenTo(this.collection, "all", this.render);
+                this.listenTo(SPOKE, "startRecording", this.toggleControls);
+                this.listenTo(SPOKE, "stopRecording", this.toggleControls);
+                _.bindAll(this);
             },
 
             render: function () {
-            	var speakersView;
+            	var speakersView, timerView;
+
                 console.log('Recording controls page rendering');
+                
                 this.$el.html(this.template());
+                
                 // Create a child speakers view that shows up in
                 // the #speaker div of our template
                 speakerView = new SPOKE.SpeakersView({
@@ -23,29 +29,65 @@
                     collection: this.collection
                 });
                 speakerView.render();
+                
+                // Bind once to a click on any speaker, so we can use that to
+                // start recording
+                this.$el.find("a.speaker").one("vclick", this.record);
+
+                // Create a child timer view that shows in #timer
+                timerView = new SPOKE.TimerView({
+                	el: this.$el.find("#timer")
+                })
+                timerView.render();
+
                 // Force jQuery Mobile to do it's stuff to the template html
                 this.$el.trigger("pagecreate");
+                
                 return this;
             },
 
             events: {
-            	"vclick #start-button": "startButton",
-                "vclick #stop-button": "stopButton"
+                "vclick #stop-button": "stop"
             },
 
-            startButton: function (e) {
-            	e.preventDefault();
-            	console.log('Starting recording audio');
-            },
-
-            stopButton: function (e) {
+            record: function(e) {
                 e.preventDefault();
-                console.log('Stopping recording audio');
+
+                console.log("Record function");
+
+            	// We only want to record if we're not already, this
+		        // could happen with dodgy click/tap/vlick handling or
+		        // slow phones and angry users
+		        if(typeof this.currentRecording === 'undefined') {
+		            // startRecording wraps the phonegap media creation api in
+		            // a promise and returns it, it also creates a new recording
+		            // model instance and saves a global reference to
+		            // the current recording media object in this.currentRecording
+		            var recordingAudio = this.startRecording();
+
+                    recordingAudio.done(function() {
+                        var message = 'Recording saved!';
+                        console.log(message);
+                        navigator.notification.alert(message);
+                    });
+
+		            // Failure - tell the user
+		            recordingAudio.fail(function(error) {
+		            	var message = 'Something went wrong recording the audio: ' + error.message;
+		                console.log(message);
+		                navigator.notification.alert(message);
+		            });
+		        }
+            },
+
+            stop: function(e) {
+            	e.preventDefault();
+            	this.stopRecording();
             },
 
             // Start recording to a new audio file in the SPOKE app's directory
             // using the Media API
-            startRecordingAudio: function () {
+            startRecording: function () {         	
                 
                 console.log('Recording Audio');
                 
@@ -54,9 +96,12 @@
                 // creatingFile and recordingAudio are promises because the Phonegap filesystem
                 // apis are asynchronous
                 var creatingFile = SPOKE.files.createFile(),
-                    recordingAudio = $.Deferred();
+                    recordingAudio = $.Deferred(),
+                    that = this; // Reference to view object
 
                 creatingFile.done( function (file) {
+
+                	var src, media, recording;
 
                     // We have a FileEntry to play with in file
                     console.log('File created: ' + file.fullPath);
@@ -64,36 +109,60 @@
                     // Create a media object to actually do the recording
                     // iOS wants the file path to be full, ie: start with file://
                     // Android just wants it to be relative
-                    var src = (device.platform.match(/(iPhone|iPod|iPad)/)) ? file.fullPath : SPOKE.audioDirectory + '/' + file.name;
-                    var media = new Media(src, recordingAudio.resolve, recordingAudio.reject);
+                    src = (device.platform.match(/(iPhone|iPod|iPad)/)) ? file.fullPath : SPOKE.config.filesDirectory + '/' + file.name;
+                    media = new Media(src, recordingAudio.resolve, recordingAudio.reject);
+                    
                     // Start the recording
                     media.startRecord();
-                    // Save the media object so that we can stop it later
-                    SPOKE.currentRecording = media;
+
+                    // Create a new model instance to hold it
+                    recording = SPOKE.recordings.create({
+                    	"name": file.name,
+                    	"path": file.fullPath
+                    });
+
+                    // Save a reference to this recording for easy access later
+                    that.currentRecording = media;
+
+                    // Trigger the global event to tell everything that recording is happening
+                	SPOKE.trigger("startRecording", {});
 
                 });
 
                 creatingFile.fail(function(error) {
-
                     // Something went wrong creating the file, and they only give us a code, great.
-                    console.log('Something went wrong creating a file, error code: ' + error.code);
-                    navigator.notification.alert('Failed to upload the file: ' + recording + ' error code was: ' + error.code);
-
+                    var message = 'Something went wrong creating a file, error code: ' + error.code;
+                    console.log(message);
+                    navigator.notification.alert(message);
                 });
 
                 return recordingAudio;
 
             },
 
-            // Stop the current recording - assumes that there is one, so check first!
-            stopRecordingAudio: function () {
+            // Stop the current recording
+            stopRecording: function () {
 
                 console.log('Stopping recording');
 
-                var media = SPOKE.currentRecording;
-                media.stopRecord();
+                console.log(JSON.stringify(this.currentRecording));
 
-            }
+                if(typeof this.currentRecording !== 'undefined') {
+                	this.currentRecording.stopRecord();
+                	// Unset the currentRecording variable
+		            delete this.currentRecording;
+                	// Trigger the global event to tell everything that recording has stopped
+                	SPOKE.trigger("stopRecording", {});
+                }
+
+            },
+
+            // Toggle the 'start recording/stop recording' buttons, and timer controls
+		    toggleControls: function () {
+		        $('#start-button').toggle();
+		        $('#stop-button').toggle();
+		        $('#timer').toggle();
+		    }
 
         })
     });
