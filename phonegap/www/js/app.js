e6d19a1f4dcdb24234c035a7aa236f8296afc13c
Steven Day <steve@mysociety.org>
2013-01-14 13:10:10 +0000

    Re-arrange event binding and calling collection.fetch so that our bound function is called when the app starts

--- phonegap/www/js/app.js
+++ phonegap/www/js/app.js
@@ -9,7 +9,12 @@
 
     // Phonegap is ready
     document.addEventListener('deviceready', pgReady.resolve, false);
-    
+
+    // For testing basics in a browser
+    if(!window.Phonegap) {
+        pgReady.resolve();
+    }
+
     // jQuery Mobile is ready
     $(document).on('mobileinit', jqmReady.resolve);
 
@@ -22,40 +27,55 @@
 
         // Everything is ready now
     	console.log('Initialising the SPOKE app');
+        
         // Initialise the app object
         _.extend(SPOKE, {
             router: new SPOKE.AppRouter(),
             recordings: new SPOKE.RecordingsCollection(),
         });
-        // Fetch initial data
-        SPOKE.recordings.fetch();
-        // Start routing
-        Backbone.history.start();
+        
         // Bind events for the whole app
+        
         // App resume events
         $(document).on('resume', function() {
             SPOKE.recordings.fetch();
         });
+
         // Reset events on the recordings collection
         SPOKE.recordings.on('reset', function(models, options) {
+
+            console.log("Recordings collection has been reset");
+
             // Get a list of the current files
             var gettingFiles = SPOKE.files.getDirectoryEntries(SPOKE.config.filesDirectory);
+
             gettingFiles.done(function (files) {
+
                 // Go over the models and find the real file, deleting the model
                 // if it's not there, ie: it's been deleted outside the app
                 SPOKE.recordings.each(function (recording) {
                     var fileExists = false;
+
                     _.each(files, function(file) {
                         if(file.name === recording.name) {
                             fileExists = true;
                         }
                     });
+
                     if(!fileExists) {
                         recording.destroy();
                     }
                 });
+
             });
+
         });
+
+        // Fetch initial data
+        SPOKE.recordings.fetch();
+        
+        // Start routing
+        Backbone.history.start();
         
     });
 
