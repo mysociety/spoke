900ded36792e99d5db012f17d89ba2d555e7699c
Steven Day <steve@mysociety.org>
2013-01-15 11:46:41 +0000

    Fix Linting errors

--- phonegap/www/js/models/recording.js
+++ phonegap/www/js/models/recording.js
@@ -1,15 +1,15 @@
 /**
  * Recording model
  * Models the data we keep on a recording:
- * 	  name: the filename of the recording on the device
+ *    name: the filename of the recording on the device
  *    path: the full path to the recording, in whatever format the device likes it
  *    speaker: the popit url of the speaker on the recording
  */
 ;(function (SPOKE, Backbone, _, $) {
-	_.extend(SPOKE, {
-		Recording: Backbone.Model.extend({
-			localStorage: new Backbone.LocalStorage("Recordings")
-		})
-	});
+    _.extend(SPOKE, {
+        Recording: Backbone.Model.extend({
+            localStorage: new Backbone.LocalStorage("Recordings")
+        })
+    });
 
 })(SPOKE, Backbone, _, $);
--- phonegap/www/js/views/timer.js
+++ phonegap/www/js/views/timer.js
@@ -13,49 +13,49 @@
             },
 
             render: function () {
-            	console.log("Timer rendering")
-            	this.$el.html(this.template());
+                console.log("Timer rendering");
+                this.$el.html(this.template());
                 return this;
             },
 
             // Start a HH:MM:SS timer which updates three <span> elements
-		    start: function () {
+            start: function () {
 
-		    	var that = this;
+                var that = this;
 
-		        console.log('Starting recording timer');
+                console.log('Starting recording timer');
 
-		        // Stop any old one, just in case
-		        this.stop();
+                // Stop any old one, just in case
+                this.stop();
 
-		        var sec = 0;
-		        this.timer = setInterval( function(){
-		            that.$el.find('#seconds').html(that.pad(++sec % 60));
-		            that.$el.find('#minutes').html(that.pad(parseInt(sec / 60, 10)));
-		            that.$el.find('#hours').html(that.pad(parseInt(sec / (60 * 60), 10)));
-		        }, 1000);
+                var sec = 0;
+                this.timer = setInterval( function(){
+                    that.$el.find('#seconds').html(that.pad(++sec % 60));
+                    that.$el.find('#minutes').html(that.pad(parseInt(sec / 60, 10)));
+                    that.$el.find('#hours').html(that.pad(parseInt(sec / (60 * 60), 10)));
+                }, 1000);
 
-		    },
+            },
 
-		    // Stop the timer and blank the values it holds
-		    stop: function() {
-		        
-		        console.log('Stopping recording timer');
+            // Stop the timer and blank the values it holds
+            stop: function() {
 
-		        if(typeof this.timer !== 'undefined') {
-		            clearInterval(this.timer);
-		        }
+                console.log('Stopping recording timer');
 
-		        this.$el.find('#seconds').html('00');
-		        this.$el.find('#minutes').html('00');
-		        this.$el.find('#hours').html('00');
+                if(typeof this.timer !== 'undefined') {
+                    clearInterval(this.timer);
+                }
 
-		    },
+                this.$el.find('#seconds').html('00');
+                this.$el.find('#minutes').html('00');
+                this.$el.find('#hours').html('00');
 
-		    // Simple function to pad a number to two digits
-		    pad: function ( val ) { 
-		        return val > 9 ? val : '0' + val; 
-		    }
+            },
+
+            // Simple function to pad a number to two digits
+            pad: function ( val ) {
+                return val > 9 ? val : '0' + val;
+            }
         })
-	});
+    });
 })(SPOKE, Backbone, _, $);
