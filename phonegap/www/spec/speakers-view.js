b88cbbb1fd490427a3c77152120b95f080813792
Steven Day <steve@mysociety.org>
2013-01-17 16:06:40 +0000

    Add saving speakers into the live recording when a speaker is clicked

--- phonegap/www/spec/speakers-view.js
+++ phonegap/www/spec/speakers-view.js
@@ -1,27 +1,27 @@
 describe('SPOKE.SpeakersView', function () {
-    var speakersView;
-    var speakers;
+    var speakersView,
+        speakers;
 
     beforeEach(function() {
-        speakers = new SPOKE.SpeakersCollection();
+        this.speakers = new SPOKE.SpeakersCollection();
         $("#stage").append('<div id="page"></div>');
-        speakersView = new SPOKE.SpeakersView({
+        this.speakersView = new SPOKE.SpeakersView({
             el: $("#page"),
-            speakers: speakers
+            speakers: this.speakers
         });
     });
 
     afterEach(function () {
-        speakers.remove();
-        speakersView.remove();
+        this.speakers.remove();
+        this.speakersView.remove();
     });
 
     it("Should have a list of speakers", function () {
-        expect(speakersView.speakers).toEqual(speakers);
+        expect(this.speakersView.speakers).toEqual(this.speakers);
     });
 
     it("Should render an empty message when there are no speakers", function () {
-        speakersView.render();
+        this.speakersView.render();
         expect($('#stage')).toContainHtml("<li>There are no speakers yet</li>");
     });
 
@@ -38,10 +38,72 @@ describe('SPOKE.SpeakersView', function () {
                 api_url: "http://example.com/speaker/2"
             }
         });
-        speakers.add(speaker1);
-        speakers.add(speaker2);
-        speakersView.render();
+        this.speakers.add(speaker1);
+        this.speakers.add(speaker2);
+        this.speakersView.render();
         expect($('#stage')).toContainHtml('<li><a href="#" class="speaker" data-api-url="http://example.com/speaker/1">speaker1</a></li>');
         expect($('#stage')).toContainHtml('<li><a href="#" class="speaker" data-api-url="http://example.com/speaker/2">speaker2</a></li>');
+    });
+
+    it("Should store the live recording, only whilst the recording is happening", function () {
+        var fakeRecording = new SPOKE.Recording({
+            name: "test.wav",
+            fullPath: "test.wav"
+        });
+        expect(this.speakersView.liveRecording).toEqual(null);
+        SPOKE.trigger("startRecording", fakeRecording);
+        expect(this.speakersView.liveRecording).toEqual(fakeRecording);
+        SPOKE.trigger("stopRecording", fakeRecording);
+        expect(this.speakersView.liveRecording).toEqual(null);
+    });
+
+    it("Should add a speaker to the liveRecording whenever a speaker is clicked", function () {
+        var fakeRecording = new SPOKE.Recording({
+            name: "test.wav",
+            fullPath: "test.wav"
+        });
+        var speaker1 = new SPOKE.Speaker({
+            name: "speaker1",
+            meta: {
+                api_url: "http://example.com/speaker/1"
+            }
+        });
+        var speaker2 = new SPOKE.Speaker({
+            name: "speaker2",
+            meta: {
+                api_url: "http://example.com/speaker/2"
+            }
+        });
+        this.speakers.add(speaker1);
+        this.speakers.add(speaker2);
+        this.speakersView.render();
+        expect(this.speakersView.liveRecording).toEqual(null);
+
+        SPOKE.trigger("startRecording", fakeRecording);
+
+        expect(this.speakersView.liveRecording.get("speakers")).toBeUndefined();
+
+        $("#stage a.speaker").first().trigger('click');
+
+        expect(this.speakersView.liveRecording.get("speakers")).toEqual([
+            {
+                timestamp: jasmine.any(Number),
+                speaker: "http://example.com/speaker/1"
+            }
+        ]);
+
+        $("#stage a.speaker").last().trigger('click');
+
+        expect(this.speakersView.liveRecording.get("speakers")).toEqual([
+            {
+                timestamp: jasmine.any(Number),
+                speaker: "http://example.com/speaker/1"
+            },
+            {
+                timestamp: jasmine.any(Number),
+                speaker: "http://example.com/speaker/2"
+            }
+        ]);
+
     });
 });
