488dacca2302af2abc669d8cbc5b2d9371d89ea2
Mark Longair <mhl@pobox.com>
2013-03-22 10:43:31 +0000

    phonegap: add a new front page that asks for a login

--- phonegap/www/js/views/login.js
+++ phonegap/www/js/views/login.js
@@ -7,18 +7,19 @@
 
             template: _.template($("#login-template").html()),
 
-            recordingsView: null,
-
-            recordings: [],
+            login_tokens: [],
 
             initialize: function (options) {
                 console.log('Login page initialising');
+		console.log('this.login_tokens is:', this.login_tokens);
+		this.login_tokens = options.login_tokens;
+		this.listenTo(this.login_tokens, "all", this.render);
                 _.bindAll(this);
             },
 
             render: function () {
                 console.log('Login page rendering');
-                this.$el.html(this.template());
+                this.$el.html(this.template({login_tokens: this.login_tokens}));
 
                 // Force jQuery Mobile to do it's stuff to the template html
                 this.$el.trigger("pagecreate");
