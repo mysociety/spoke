40b2a6ec646ee7e2acb60b41f86255bcd3cea4e6
Mark Longair <mhl@pobox.com>
2013-03-22 11:13:15 +0000

    Add the forgotten models and collections for LoginToken

---
+++ phonegap/www/js/collections/login_tokens.js
@@ -0,0 +1,27 @@
+/* -*- mode: espresso; espresso-indent-level: 4; indent-tabs-mode: nil -*- */
+/**
+ * LoginTokens Collection.
+ * A collection of LoginToken models
+ */
+
+ ;(function (SPOKE, Backbone, _, $) {
+     _.extend(SPOKE, {
+	 LoginTokensCollection: Backbone.Collection.extend({
+	     model: SPOKE.LoginToken,
+	     localStorage: new Backbone.LocalStorage("LoginTokens"),
+
+             initialize: function () {
+                 // Reset events on the login tokens collection
+                 console.log("LoginTokens collection initialising");
+                 _.bindAll(this);
+             },
+
+             refreshLocalStorage: function () {
+                 this.localStorage = new Backbone.LocalStorage("LoginTokens");
+                 _.each(this.models, function(model) {
+                     model.refreshLocalStorage();
+                 });
+             }
+	 })
+     })
+})(SPOKE, Backbone, _, $);
---
+++ phonegap/www/js/models/login_token.js
@@ -0,0 +1,48 @@
+/* -*- mode: espresso; espresso-indent-level: 4; indent-tabs-mode: nil -*- */
+/**
+ * Login token model
+ * Models one of the three-word login tokens that has been previously used
+ */
+;(function (SPOKE, Backbone, _, $) {
+    _.extend(SPOKE, {
+        LoginToken: Backbone.Model.extend({
+
+            validate: function (attributes) {
+                if (!attributes.three_word_token) {
+                    return "A LoginToken must at least have three_word_token set";
+                }
+            },
+
+            initialize: function (attributes, options) {
+                _.bindAll(this);
+            },
+
+            defaults: {
+                instance: null,
+                user: null,
+                cookie: null
+            },
+
+            authenticated: function () {
+                return ! (this.instance === null && this.user === null)
+            }
+
+            toString: function () {
+                if this.authenticated() {
+                    return this.instance + " as " + this.user;
+                } else {
+                    return "[unused] " + this.three_word_token;
+                }
+            },
+
+            localStorage: new Backbone.LocalStorage("LoginTokens"),
+
+            refreshLocalStorage: function () {
+                this.localStorage = new Backbone.LocalStorage("LoginToken");
+            },
+
+
+        })
+    });
+
+})(SPOKE, Backbone, _, $);
