64bc8a02eacbce819a44d30bab45b252ff542037
Steven Day <steve@mysociety.org>
2013-03-25 17:07:24 +0000

    Fix some indentation

--- phonegap/www/js/views/login.js
+++ phonegap/www/js/views/login.js
@@ -11,11 +11,11 @@
 
             initialize: function (options) {
                 console.log('Login page initialising');
-		console.log('this.login_tokens is:', this.login_tokens);
-		this.login_tokens = options.login_tokens;
-		this.listenTo(this.login_tokens, "all", this.render);
-                _.bindAll(this);
-            },
+                console.log('this.login_tokens is:', this.login_tokens);
+                this.login_tokens = options.login_tokens;
+                this.listenTo(this.login_tokens, "all", this.render);
+                    _.bindAll(this);
+                },
 
             render: function () {
                 console.log('Login page rendering');
